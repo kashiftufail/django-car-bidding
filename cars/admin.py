bd0e658459fea8abeaec48d4297e264c6e4ebc1c
kashif <kashif.tufail17@gmail.com>
2025-07-10 18:55:19 +0500

    delete extra code

--- cars/admin.py
+++ cars/admin.py
@@ -3,35 +3,8 @@ from .models import Car
 from images.models import Image
 from django.utils.safestring import mark_safe
 
-
-# class ImageInline(admin.TabularInline):
-#     model = Image
-#     fields = ['preview', 'file', 'alt_text', 'uploaded_by']  # Add 'preview'
-#     readonly_fields = ['preview', 'uploaded_by']  # Add 'preview'
-#     extra = 1
-#     can_delete = True
-
-#     def preview(self, obj):
-#         if obj.file:
-#             return mark_safe(f'<img src="{obj.file.url}" width="100" height="60" style="object-fit:cover;border-radius:4px;" />')
-#         return "No Image"
-#     preview.short_description = "Preview"
-
-#     def save_formset(self, request, form, formset, change):
-#         hhhhhhhhh
-#         breakpoint()  # Debugging line to check if save_formset is called
-#         instances = formset.save(commit=False)
-#         breakpoint()  # Debugging line to check if save_formset is called
-#         for instance in instances:
-#             if not instance.uploaded_by_id:
-#                 breakpoint()  # Debugging line to check if uploaded_by is set
-#                 instance.uploaded_by = request.user.uerprofile
-#             instance.save()
-#         formset.save_m2m()
-
 from images.forms import ImageInlineFormSet
 
-# from images.forms import ImageInlineFormSet
 
 class ImageInline(admin.TabularInline):
     model = Image
